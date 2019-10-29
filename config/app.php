218d7f572b4f8f9e71f73394a7930e8f0a11f96b
Curso Git <marcooperdomo@gmail.com>
2019-10-29 17:35:31 -0600

    Instalación de paquetes

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /* Laravel Collective */
+        Collective\Html\HtmlServiceProvider::class,
+        /* Roles y Permisos con Shinobi */
+        Caffeinated\Shinobi\ShinobiServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /* Laravel Collective */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
