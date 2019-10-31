dd80c91ab00c1e6a742df98fcb63843a49ea2217
Curso Git <marcooperdomo@gmail.com>
2019-10-31 12:01:46 -0600

    Seeds, migración y factory

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+// Importado
+use Caffeinated\Shinobi\Models\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
+        factory( App\User::class, 20 )->create();
+
+        Role::create( [ 
+            
+            'name'      => 'Admin',
+            'slug'      => 'admin',
+            'special'   => 'all-access',
+
+         ] );
+
     }
 }
