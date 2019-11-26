803e10b50ba99d311db6f80b7d2d2acabda5cd43
Curso Git <marcooperdomo@gmail.com>
2019-11-26 17:33:42 -0600

    Controlador y vistas para roles

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Caffeinated\Shinobi\Models\Role;
+// Importado
+use Caffeinated\Shinobi\Models\Permission;
 
 class RoleController extends Controller
 {
@@ -13,7 +16,11 @@ class RoleController extends Controller
      */
     public function index()
     {
-        //
+        
+        $roles = Role::paginate();
+
+        return view( 'roles.index', compact( 'roles' ) );
+
     }
 
     /**
@@ -23,7 +30,9 @@ class RoleController extends Controller
      */
     public function create()
     {
-        //
+        
+        return view( 'roles.create' );
+
     }
 
     /**
@@ -34,51 +43,71 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        $products = Product::create( $request->all() );
+
+        return redirect()->route( 'products.edit', $product->id )->with( 'info', 'Producto guardado con éxito' );
+
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Role  $role
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Role $role)
     {
-        //
+        
+        return view( 'roles.show', compact( 'role' ) );
+
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Role  $role
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Role $role)
     {
-        //
+        $roles = Role::get();
+
+        return view( 'roles.edit', compact( 'role', 'roles' ) );
+
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Role  $role
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Role $role)
     {
-        //
+        /* Actualizar usuario */
+        $role->update( $request->all() );
+
+        /* Actualizar roles */
+        $role->roles()->sync( $request->get( 'roles' ) );
+
+        return redirect()->route( 'roles.edit', $role->id )->with( 'info', 'Usuario actualizado con éxito' );
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Role  $role
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Role $role)
     {
-        //
+        
+        $role->delete();
+
+        return back()->with( 'info', 'Eliminado correctamente' );
+
     }
 }
